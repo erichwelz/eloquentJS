7d86823c32f7026dfa69e9b232afb52840e2ab73
Erich Welz <erichwelz@gmail.com>
2014-11-06 19:16:31 -0500

    ch.04 function calling a function

---
+++ ch04-objects-and-arrays.js
@@ -0,0 +1,58 @@
+// sum of a range
+
+function range(start, end) {
+  var arr = [];
+  var index = 0;
+  for (start; start <= end; start++) {
+    arr[index] = start;
+    index += 1;
+  }
+  return arr;
+}
+
+function sum(arr) {
+  var total = 0
+  for (var i = 1; i <= arr.length; i++) {
+  total += arr[i - 1];
+  }
+  return total;
+}
+
+console.log(sum(range(1, 10)));
+
+//sum of range v2
+
+function range(start, end) {
+  var arr = [];
+  for (var i = start; i <= end; i++) {
+    arr.push(i)
+  }
+  return arr;
+}
+
+function sum(arr) {
+  var total = 0
+  for (var i = 0; i < arr.length; i++) {
+  total += arr[i];
+  }
+  return total;
+}
+
+console.log(sum(range(1, 10)));
+
+//range accepts negative steps
+
+function range(start, end, step) {
+  if (step == null) step = 1;
+  var arr = [];
+  if (step >= 1) {
+    for (var i = start; i <= end; i += step)
+      arr.push(i);
+    } else {
+    for (var i = start; i >= end; i += step)
+      arr.push(i);
+   }
+  return arr;
+  }
+
+console.log(range(5, 2, -1))
