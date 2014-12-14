2da4dc05f10d4e7f0df9605080579fdcc9c77e79
Erich Welz <erichwelz@gmail.com>
2014-12-14 15:33:18 -0500

    filter with map example

--- ch05_higher_order_functions.js
+++ ch05_higher_order_functions.js
@@ -7,3 +7,36 @@ console.log(arrays.reduce(function(output, current) {
   return output.concat(current);
 }, []));
 // → [1, 2, 3, 4, 5, 6]
+
+// Reduce the multiplication of a series numbers
+
+nums = [1,2,3,4,5,6];
+
+console.log(nums.reduce(function(a,b){
+  return a * b;
+  })
+);
+
+
+//mother-child age difference
+
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+var differences = ancestry.filter(function(person) {
+    //with known mother
+  return byName[person.mother] != null;
+}).map(function(person) {
+    //gap between birth of mother and offspring
+  return person.born - byName[person.mother].born;
+});
+
+console.log(average(differences));
+// → 31.2
