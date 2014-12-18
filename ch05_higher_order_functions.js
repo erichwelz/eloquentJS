b672b6cd7a043a00b9be675dd6d8b39676484d98
Erich Welz <erichwelz@gmail.com>
2014-12-18 11:59:03 -0700

    rest of ch.05 exercises

--- ch05_higher_order_functions.js
+++ ch05_higher_order_functions.js
@@ -40,3 +40,63 @@ var differences = ancestry.filter(function(person) {
 
 console.log(average(differences));
 // → 31.2
+
+Life expectancy by century:
+
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+function groupBy(array, groupOf) {
+var groups = {};
+  array.forEach(function(element) {
+    var groupName = groupOf(element);
+    if (groupName in groups)
+      groups[groupName].push(element);
+    else
+      groups[groupName] = [element];
+  });
+  return groups;
+}
+
+var byCentury = groupBy(ancestry, function(person) {
+  return Math.ceil(person.died / 100)
+});
+
+for (var century in byCentury) {
+  var ages = byCentury[century].map(function(person) {
+    return person.died - person.born;
+  });
+  console.log(century + ": " + average(ages));
+}
+
+//Every and some methods
+
+// Your code here.
+function every(arr, check) {
+  for (var i = 0; i < arr.length; i++) {
+    if (!check(arr[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+function some(arr, check) {
+  for (var i = 0; i < arr.length; i++) {
+    if (check(arr[i])) {
+      return true;
+    }
+  }
+  return false;
+};
+
+console.log(every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
+// → false
