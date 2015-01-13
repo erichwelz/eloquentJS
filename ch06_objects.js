ebfb574fed779b5d68f0cecc51649ea15e28d2a5
Erich Welz <erichwelz@gmail.com>
2015-01-12 20:49:54 -0500

    ch06 Object surrounding object

--- ch06_objects.js
+++ ch06_objects.js
@@ -25,3 +25,36 @@ console.log(new Vector(1, 2).minus(new Vector(2, 3)));
 // → Vector{x: -1, y: -1}
 console.log(new Vector(3, 4).length);
 // → 5
+
+//Another Cell wrapping Textcell prototype defined in chapter
+
+// Your code here.
+function StretchCell(inner, width, height) {
+  this.inner = inner;
+  this.width = width;
+  this.height = height;
+}
+
+var sc = new StretchCell(new TextCell("abc"), 1, 2);
+
+StretchCell.prototype.minWidth = function(){
+  return Math.max(this.width, this.inner.minWidth());
+};
+
+StretchCell.prototype.minHeight = function(){
+  return Math.max(this.height,this.inner.minHeight());
+};
+
+StretchCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width, height)
+};
+
+console.log(sc.minWidth());
+// → 3
+console.log(sc.minHeight());
+// → 2
+console.log(sc.draw(3, 2));
+// → ["abc", "   "]
+
+//Sequence Interface
+
